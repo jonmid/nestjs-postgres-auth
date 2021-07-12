4a3e9c75f3867815d2ce78c59340260c81f30bc1
JonMid <10jonmid@gmail.com>
2021-07-12 10:10:09 -0500

    [add] decorators custom guard all controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,30 +1,33 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Public } from './auth/decorators/public.decorator';
 
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { AppService } from './app.service';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @UseGuards(ApiKeyGuard)
   @Get('nuevo')
   newEndpoint() {
     return 'yo soy nuevo';
   }
 
+  @Public()
   @Get('/ruta/')
   hello() {
     return 'con /sas/';
   }
 
-  @Get('tasks')
-  tasks() {
+  @Get('/tasks/')
+  getTasks() {
     return this.appService.getTasks();
   }
 }
